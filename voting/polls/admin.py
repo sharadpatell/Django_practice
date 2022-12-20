400bf25d2eb64524188ffaac5ccd3d71bb41b6a9
sharad <patelsharad1485@gmail.com>
2022-12-20 10:20:15 +0530

    djnago

---
+++ voting/polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+# Register your models here.
+from .models import Question, Choice
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+admin.site.site_header = "Voting Admin"
+admin.site.site_title = "Voting Admin Area"
+admin.site.index_title = "Welcome to the Voting Admin Area"
+
+
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {
+		'fields': ['pub_date'], 'classes': ['collapse']}), ]
+	inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
